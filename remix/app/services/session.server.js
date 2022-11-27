67ef1031f056615cf04fdfde7683613fe2c1104f
Laurie Voss <github@seldo.com>
2022-11-26 16:07:10 -0800

    Actually secret cookie secret

--- remix/app/services/session.server.js
+++ remix/app/services/session.server.js
@@ -7,7 +7,7 @@ export let sessionStorage = createCookieSessionStorage({
     sameSite: "lax", // this helps with CSRF
     path: "/", // remember to add this so the cookie will work in all routes
     httpOnly: true, // for security reasons, make this cookie http only
-    secrets: ["fdasfasdfasdffas"], // replace this with an actual secret
+    secrets: [process.env.COOKIE_SECRET], // replace this with an actual secret
     secure: process.env.NODE_ENV === "production", // enable this in prod only
   },
 });
