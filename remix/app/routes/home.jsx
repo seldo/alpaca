830c8b45549fab3a3bf47d6a7ca8f61eb3daf9ab
Laurie Voss <github@seldo.com>
2022-11-30 07:18:57 -0800

    Y U NO FIND TWEET COMPONENT

--- remix/app/routes/home.jsx
+++ remix/app/routes/home.jsx
@@ -3,7 +3,7 @@ import { useLoaderData, useFetcher } from "@remix-run/react";
 import authenticator from "../services/auth.server";
 import * as mastodon from "../models/tweets.server";
 import stylesRoot from "../styles/root.css";
-import Tweet from "../shared/components/tweet"
+//import Tweet from "../shared/components/tweet"
 
 export const links = () => {
   return [
@@ -18,7 +18,37 @@ export const loader = async ({request}) => {
     let user = await mastodon.getOrCreateUser(authUser)
     let timeline = await mastodon.getTimeline(authUser,{ hydrate: true })
     return { user, timeline }
-}  
+}
+
+const Tweet = (t) => {
+  if(t.reblog !== null) {
+      return <div className="reblog">
+          <div className="reblogNotice">{t.account.display_name || "@" + t.account.username} reblogged</div>
+          {Tweet(t.reblog)}
+      </div>
+  } else {
+      return <li className="tweet flex flex-row w-full" key={t.id}>
+          <div className="gutter">
+          <div className="authorAvatar">
+              <img src={t.account.avatar} />
+          </div>
+          </div>
+          <div className="tweetBody grow">
+          <div className="author">
+              <span className="displayName">{t.account.display_name}</span>
+              <span className="username">@{t.account.acct}</span>
+          </div>
+          <div className="status" dangerouslySetInnerHTML={{__html: t.content}} />
+          <div className="reactions flex flex-row place-content-between w-full">
+              <div className="replies">{t.replies_count ? t.replies_count : ''}</div>
+              <div className="reblogs">{t.reblogs_count ? t.reblogs_count : ''}</div>
+              <div className="favorites">{t.favourites_count ? t.favourites_count : ''}</div>
+              <div className="share"><span>Share</span></div>
+          </div>
+          </div>
+      </li>
+  }
+}
 
 export default function Index() {
   const loaderData = useLoaderData();
