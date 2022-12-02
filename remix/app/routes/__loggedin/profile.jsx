386a34c73fbc26ec0e2293f715b061c110413c08
Laurie Voss <github@seldo.com>
2022-12-01 17:54:36 -0800

    Redirects /profile to your profile, closes #12

--- remix/app/routes/__loggedin/profile.jsx
+++ remix/app/routes/__loggedin/profile.jsx
@@ -1,6 +1,9 @@
 import { redirect } from "@remix-run/node"; 
+import authenticator from "~/services/auth.server";
+import * as mastodon from "~/models/tweets.server";
 
-export default Profile = async ({user}) => {
-    console.log(user)
-    return;
+export const loader = async ({request}) => {
+    let authUser = await authenticator.isAuthenticated(request)
+    let user = await mastodon.getOrCreateUserFromData(authUser)
+    return redirect(`/u/${user.username}@${user.instance}`)
 }
