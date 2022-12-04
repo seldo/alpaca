add64880c9bf6da0bd974d66a9162c565fec8f62
Laurie Voss <github@seldo.com>
2022-12-03 21:59:32 -0800

    Timeline should also use authenticateAndRefresh

--- remix/app/routes/__loggedin/timeline.js
+++ remix/app/routes/__loggedin/timeline.js
@@ -1,12 +1,9 @@
-import authenticator from "~/services/auth.server";
+import { authenticateAndRefresh } from "~/services/auth.server";
 import * as mastodon from "~/models/tweets.server";
 
 export const loader = async ({request}) => {
     console.log("/timeline called")
-    let authUser = await authenticator.isAuthenticated(request, {
-        failureRedirect: "/auth/mastodon?fromtimelinefeed",
-        throwOnError: true
-    })
+    let authUser = await authenticateAndRefresh(request)
     const url = new URL(request.url);
     let minId = url.searchParams.get("minId");    
     if(minId == "null") minId = null
