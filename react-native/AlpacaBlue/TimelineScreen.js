26434fe8e0079e81fcf5d3b3ee1b4dc3dbddb68a
Laurie Voss <github@seldo.com>
2022-12-15 11:09:44 -0800

    Virtualized lists n shit

--- react-native/AlpacaBlue/TimelineScreen.js
+++ react-native/AlpacaBlue/TimelineScreen.js
@@ -1,45 +1,67 @@
-import { StyleSheet, Text, View, Button, SafeAreaView, TextInput } from 'react-native';
+import { SafeAreaView, View, VirtualizedList, StyleSheet, Text, useWindowDimensions } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
+import RenderHtml from 'react-native-render-html'
 
 export const TimelineScreen = ({navigation}) => {
 
-    const [data,setData] = useState([])
+    const [allPosts,setAllPosts] = useState([])
 
+    const fetchTimeline = async (minId = null,maxId = null) => {
+        let auth = JSON.parse(await AsyncStorage.getItem('auth'))
+        let timelineUrl = new URL("https://seldo.dev/api/v1/timelines/home")
+        timelineUrl.searchParams.append('limit',40)
+        let res = await fetch(timelineUrl,{
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${auth.accessToken}`
+            }            
+        })
+        let timeline = await res.json()   
+        console.log("Timeline items:",timeline.length)
+        console.log("Timeline content:",timeline.map( (p,index) => index + ":" + p.content))
+        return timeline
+    }
+
+    // initialize to the top of the timeline
     useEffect( () => {
         (async () => {
-            let auth = JSON.parse(await AsyncStorage.getItem('auth'))
-            console.log("auth is",auth)
-            let res = await fetch('https://seldo.dev/api/v1/timelines/home',{
-                method: "GET",
-                headers: {
-                    "Authorization": `Bearer ${auth.accessToken}`
-                }            
-            })
-            let timeline = await res.json()   
-            console.log(timeline)
-            setData(timeline)
+            let timeline = await fetchTimeline()
+            setAllPosts(timeline)
         })();
     },[])
 
-    useEffect( () => {
-       setData(data)
-    },[data])
+    let contentWidth = useWindowDimensions().width
+
+    const getItem = (data, index) => data[index];
+    const getItemCount = (data) => data.length;
+    const Post = ({content}) => (
+        <View style={styles.item}>
+          <RenderHtml
+                    contentWidth={contentWidth}
+                    source={{html:content}}
+                    styles={{
+                        foregroundColor: 'red',
+                        textAlign: 'left',
+                        borderWidth: 1
+                    }}
+                />
+        </View>
+      );
     
     return (
         <View style={styles.container}>
             <Text>This will be a timeline.</Text>
-            {
-                data ? data.map( (p) => {
-                    return <View>
-                        <Text>Yo.</Text>
-                        <Text>{p.content}</Text>
-                    </View>
-                }) : <Text>Nothing yet.</Text>
-            }
-            {
-                data ? <Text>Yes</Text> : <Text>No</Text>
-            }
+            <SafeAreaView>
+                <VirtualizedList
+                    data={allPosts}
+                    initialNumToRender={20}
+                    renderItem={({ item }) => <Post content={item.content} />}
+                    keyExtractor={item => item.id}
+                    getItemCount={getItemCount}
+                    getItem={getItem}
+                />
+            </SafeAreaView>
             <Text>What</Text>
         </View>
     );
